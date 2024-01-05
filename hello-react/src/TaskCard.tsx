b2bcc4a10a98eb23560662e45aab99a05fde047c
eat_code_sleep <141677292+i0am0arunava@users.noreply.github.com>
2024-01-05 16:23:13 +0530

    Update TaskCard.tsx

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,22 +1,23 @@
 import React from "react"
 import "./TaskCard.css";
 interface User {
-  title:string,
-  dueDate?:string,
-  completedAtDate?:string,
-  assigneeName:string,
-  completed:boolean
+  title: string;
+  dueDate?: string; 
+  completedAtDate?: string;
+  assigneeName: string;
+
+  completed:boolean;
 
 }
 
-const TaskCard :React.FC<User>= ({title,dueDate,completedAtDate,assigneeName,completed}) => {
-  console.log()
-  if (completed)
+const TaskCard :React.FC<User>= (props) => {
+  console.log(props)
+  if (props.completed)
     return (
       <div className="TaskItem  border-black border rounded-md p-4">
-        <h1 className="text-xl font-bold">{title}</h1>
-        <p>Completed on: {completedAtDate}</p>
-        <p>Assignee: {assigneeName}</p>
+        <h1 className="text-xl font-bold">{props.title}</h1>
+        <p>Completed on: {props.completedAtDate}</p>
+        <p>Assignee: {props.assigneeName}</p>
       </div>
 
     )
@@ -25,9 +26,9 @@ const TaskCard :React.FC<User>= ({title,dueDate,completedAtDate,assigneeName,com
 
 
       <div className="TaskItem  border-black border rounded-md p-4">
-        <h1 className="text-xl font-bold">{title}</h1>
-        <p>Due on: {dueDate}</p>
-        <p>Assignee: {assigneeName}</p>
+        <h1 className="text-xl font-bold">{props.title}</h1>
+        <p>Due on: {props.dueDate}</p>
+        <p>Assignee: {props.assigneeName}</p>
       </div>
     )
 }
