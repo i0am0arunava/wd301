db0c404e8ecaf0d14ded6e40cb4b79f7f8d7dc5a
arunava369369 <arunavapari536@gmail.com>
2024-01-05 10:11:24 +0530

    l2

---
+++ hello-react/src/TaskCard.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+import "./TaskCard.css";
+interface User {
+  title:string,
+   dueDate:string,
+  completedAtDate:string,
+  assigneeName:string | number,
+  completed:boolean
+
+}
+
+const TaskCard :React.FC<User>= ({title,dueDate,completedAtDate,assigneeName,completed} : User) => {
+  console.log()
+  if (completed)
+    return (
+      <div className="TaskItem  border-black border rounded-md p-4">
+        <h1 className="text-xl font-bold">{title}</h1>
+        <p>Completed on: {completedAtDate}</p>
+        <p>Assignee: {assigneeName}</p>
+      </div>
+
+    )
+  else
+    return (
+
+
+      <div className="TaskItem  border-black border rounded-md p-4">
+        <h1 className="text-xl font-bold">{title}</h1>
+        <p>Due on: {dueDate}</p>
+        <p>Assignee: {assigneeName}</p>
+      </div>
+    )
+}
+export default TaskCard
+
+
+
