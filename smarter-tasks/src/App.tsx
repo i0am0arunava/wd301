dc0ef012b39acb081d91cbecb1b739df8b4d6480
arunava369369 <arunavapari536@gmail.com>
2024-02-12 02:42:32 +0530

    l9

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,26 +1,30 @@
-// src/App.tsx
-
 import { useContext } from "react";
 import { RouterProvider } from "react-router-dom";
-import "./App.css";
-import router from "./routes"
+import router from "./routes";
 import { ThemeContext } from "./context/theme";
-
-// To do that, first I'll import the `ProjectsProvider` in the `App` component.
-
 import { ProjectsProvider } from "./context/projects/context";
 import { MembersProvider } from "./context/members/context";
-// Then I'll wrap the RouterProvider component with the <ProjectsProvider> component.
+import {  } from "./context/comment/context";
+
 const App = () => {
-  const { theme } = useContext(ThemeContext)
+  const { theme } = useContext(ThemeContext);
+
   return (
-     <div className={`h-screen w-full mx-auto py-2 ${theme === "dark" ? "dark" : ""}`}>
-      <MembersProvider>
+    <div
+      className={`h-screen w-full mx-auto py-2 ${
+        theme === "dark" ? "dark" : ""
+      }`}
+    >
+      
       <ProjectsProvider>
-        <RouterProvider router={router} />
+        <MembersProvider>
+          <>
+          <RouterProvider router={router} />
+          </>
+        </MembersProvider>
       </ProjectsProvider>
-      </MembersProvider>
     </div>
   );
-}
+};
+
 export default App;
